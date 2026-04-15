6c727b1495b406a673f616071b0413393eeb570f
Kilo Loco <kyle@kiloloco.com>
2026-04-14 18:34:23 -0700

    Initial Commit

---
+++ CertPinningDemo/CertPinningDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertPinningDemoApp.swift
+//  CertPinningDemo
+//
+//  Created by Kilo Loco on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CertPinningDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
